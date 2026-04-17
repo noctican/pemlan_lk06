023c99b2baa1977296e1ad9560ec1fc55819a1c2
BagasZakaria <bagaszakaria1004@gmail.com>
2026-04-17 21:15:01 +0700

    Menambahkan penjelasan Book.java

--- src/entities/Book.java
+++ src/entities/Book.java
@@ -3,12 +3,15 @@ package src.entities;
 public class Book extends BaseEntity {
     private String kode, judul, jenis;
 
+    // Empty constructor sebagai pancingan awal saat DataListManager membaca file txt.
     public Book() {}
     public Book(String kode, String judul, String jenis) {
         this.kode = kode;
         this.judul = judul;
         this.jenis = jenis;
     }
+    
+    //Getter
     public String getId() {
         return kode;
     }
@@ -18,9 +21,13 @@ public class Book extends BaseEntity {
     public String getJenis() {
         return jenis;
     }
+
+    //Untuk menggabungkan data menjadi sebaris teks agar mudah disimpan ke dalam file txt.
     public String toDataString() {
         return kode + DELIMITER + judul + DELIMITER + jenis;
     }
+
+    //Untuk memecah baris teks dari file txt untuk dimasukkan kembali ke dalam variabel objek.
     public void fromDataString(String data) {
         String[] parts = data.split(DELIMITER);
         if (parts.length >= 3) {
