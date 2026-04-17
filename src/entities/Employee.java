debda5112bde47bdd03e710c21c5bdb5753d8fe9
Candra Andika Putra <candrandikarpl1.kanesa@gmail.com>
2026-04-17 21:06:39 +0700

    mod: update class employee

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,8 +1,44 @@
 package src.entities;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Employee extends BaseEntity {
     private String nip, nama, tanggalLahir, passwordHash;
 
     public Employee() {}
-    public Employee(String nip, String nama, String tanggalLahir, String passwordHash, boolean isHash) {}
+    public Employee(String nip, String nama, String tanggalLahir, String passwordHash, boolean isHash) {
+        this.nip = nip; this.nama = nama; this.tanggalLahir = tanggalLahir;
+        // pengecekan passwordHash (untuk update tanpa mengubah password, maka isHash = true)
+        this.passwordHash = isHash ? passwordHash : hashPassword(passwordHash);
+    }
+
+    public String getId() { return nip; }
+    public String getNama() { return nama; }
+    public String getTanggalLahir() { return tanggalLahir; }
+    public String getPasswordHash() { return passwordHash; }
+
+    public String toDataString() { return nip + DELIMITER + nama + DELIMITER + tanggalLahir + DELIMITER + passwordHash; }
+    public void fromDataString(String data) {
+        String[] parts = data.split(DELIMITER);
+        // memastikan data yang ada pada file tidak kurang
+        if (parts.length >= 4) { nip = parts[0]; nama = parts[1]; tanggalLahir = parts[2]; passwordHash = parts[3]; }
+    }
+    
+    public static String hashPassword(String password) {
+        try {
+            // hashing password
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // throw exception jika algoritma hashing tidak ditemukan
+            throw new RuntimeException("Error hashing password", e);
+        }
+    }
 }
