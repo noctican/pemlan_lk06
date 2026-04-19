ab8ae277048864b7ea2aeae6194540201e3497e6
BagasZakaria <bagaszakaria1004@gmail.com>
2026-04-19 19:15:49 +0700

    Melengkapi Transaction.java

--- src/entities/Transaction.java
+++ src/entities/Transaction.java
@@ -7,38 +7,57 @@ public class Transaction extends BaseEntity {
     private LocalDate tglPinjam, tglKembali;
     private int status; // 0: belum dikembalikan, 1: dikembalikan
 
-    // Cosntructor
+    // Cosntructor kosong sebagai syarat untuk membaca file.
     public Transaction() {}
     public Transaction(String kodeTransaksi, String nis, String kodeBuku, LocalDate tglPinjam) {
-        this.kodeTransaksi = kodeTransaksi; this.nis = nis; this.kodeBuku = kodeBuku;
-        this.tglPinjam = tglPinjam; this.tglKembali = null; this.status = 0;
+        this.kodeTransaksi = kodeTransaksi; 
+        this.nis = nis; 
+        this.kodeBuku = kodeBuku;
+        this.tglPinjam = tglPinjam; 
+        this.tglKembali = null; 
+        this.status = 0;
     }
 
     // Getter
-    public String getId() {return kodeTransaksi;}
-    public String getNis() { return nis; }
-    public String getKodeBuku() { return kodeBuku; }
-    public int getStatus() { return status; }
-    public LocalDate getTglPinjam() { return tglPinjam; }
+    public String getId() {
+        return kodeTransaksi;
+    }
+    public String getNis() { 
+        return nis; 
+    }
+    public String getKodeBuku() { 
+        return kodeBuku; 
+    }
+    public int getStatus() { 
+        return status; 
+    }
+    public LocalDate getTglPinjam() { 
+        return tglPinjam; 
+    }
 
-    // Method
+    // Method untuk mengubah status menjadi sudah dikembalikan dan langsung mencatat tanggal hari ini.
     public void kembalikanBuku() {
         this.status = 1;
         this.tglKembali = LocalDate.now();
     }
 
+    //Untuk menyusun data agar siap disimpan ke dalam file txt.
     public String toDataString() {
         String tglKembaliStr = (tglKembali == null) ? "null" : tglKembali.toString();
         return kodeTransaksi + DELIMITER + nis + DELIMITER + kodeBuku + DELIMITER + tglPinjam.toString() + DELIMITER + tglKembaliStr + DELIMITER + status;
     }
 
+    //Untuk membaca teks sebaris dari file txt lalu memotong-motongnya.
     public void fromDataString(String data) {
         String[] parts = data.split(DELIMITER);
         if (parts.length >= 6) {
-            kodeTransaksi = parts[0]; nis = parts[1]; kodeBuku = parts[2];
-            tglPinjam = LocalDate.parse(parts[3]);
-            tglKembali = parts[4].equals("null") ? null : LocalDate.parse(parts[4]);
-            status = Integer.parseInt(parts[5]);
+            this.kodeTransaksi = parts[0]; 
+            this.nis = parts[1]; 
+            this.kodeBuku = parts[2];
+            this.tglPinjam = LocalDate.parse(parts[3]);
+            // Jika di dalam file terdapat tulisan "null", berarti tanggal kembalinya dibiarkan kosong (bernilai null).
+            this.tglKembali = parts[4].equals("null") ? null : LocalDate.parse(parts[4]);
+            this.status = Integer.parseInt(parts[5]);
         }
     }
 }
